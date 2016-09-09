1eff395d42a3d9216abea3883ac1a69804569e1a
[rpc01] <[rpratc@gmail.com]>
2016-09-09 17:44:10 +0200

    dwload JSON from FRED

--- src/forecast.R
+++ src/forecast.R
@@ -1,27 +1,60 @@
 library('ProjectTemplate')
 load.project()
 
-ts1 <- readWorksheetFromFile("data/PAYEMS.xls",
-                            sheet=1,
-                            startRow = 11,
-                            endCol = 2) #thousand of employed
-ts1<-as.timeSeries(ts1)
-ts2<-as.timeSeries(CPIAUCSL)
-ts3<-as.timeSeries(realgdp)
-ts4<-as.timeSeries(unemployment)
+#json config to download data from fred
+api_key<-"&api_key=9912ee381321a596b999f26cfa5da2a5"
+url<-"https://api.stlouisfed.org/fred/series/observations?"
+file_type<-"&file_type=json"
 
-tail(ts1)
-tail(ts2)
-tail(ts3)
-tail(ts4)
+#Change in nonfarm payroll employment
+s1<-"series_id=PAYEMS" #seasonally adjusted monthly in nonfarm payroll employment to the nearest thousand persons
+s1<-fromJSON(paste0(url, s1, api_key,file_type))
+s1_df<-as.data.frame(s1)
+#names(s1_df)
+charvec<-as.Date(as.character(s1_df[,15]))
+data<-as.numeric(s1_df[,16])
+s1<-as.timeSeries(data, charvec)
+s1<-diff(s1,1) #to compute monthly change
 
-#employment is the monthly increase
-ts1<-(diff(ts1,1))
 
-fit1 <- auto.arima(ts1)
-fit2 <- auto.arima(ts2)
-fit3 <- auto.arima(ts3)
-fit4 <- auto.arima(ts4)
+#Inflation
+s2<-"series_id=CPIAUCSL"    #"seasonally adjusted annualized monthly percentage change in headline CPI
+                            #rounded to the nearest tenth of a percent"
+s2<-fromJSON(paste0(url, s2, api_key,file_type))
+s2_df<-as.data.frame(s2)
+charvec<-as.Date(as.character(s2_df[,15]))
+data<-as.numeric(s2_df[,16])
+s2<-as.timeSeries(data, charvec)
+s2<-((Delt(s2, type="log"))+1)^12-1 #annualized percentage change
+
+
+#Real GDP Growth
+s3<-"series_id=A191RL1Q225SBEA"     #"seasonally adjusted annualized quarter-to-
+                                    #quarter percentage change advance estimate 
+                                    #of GDP from the previous final estimate of 
+                                    #GDP, rounded to the nearest tenth of a percent"
+s3<-fromJSON(paste0(url, s3, api_key,file_type))
+s3_df<-as.data.frame(s3)
+charvec<-as.Date(as.character(s3_df[,15]))
+data<-as.numeric(s3_df[,16])
+s3<-as.timeSeries(data, charvec)
+
+#"Unemployment 
+s4<-"series_id=UNRATE"      #is the seasonally adjusted level of the headline
+                            #unemployment rate in percentage points rounded to 
+                            #the nearest tenth of a percent")
+s4<-fromJSON(paste0(url, s4, api_key,file_type))
+s4_df<-as.data.frame(s4)
+charvec<-as.Date(as.character(s4_df[,15]))
+data<-as.numeric(s4_df[,16])
+s4<-as.timeSeries(data, charvec)
+
+# FORECAST
+
+fit1 <- auto.arima(s1)
+fit2 <- auto.arima(s2)
+fit3 <- auto.arima(s3)
+fit4 <- auto.arima(s4)
 
 ts1_cast<-forecast(fit1, 1)
 ts2_cast<-forecast(fit2, 1)
@@ -29,8 +62,7 @@ ts3_cast<-forecast(fit3, 1)
 ts4_cast<-forecast(fit4, 1)
 
 parameter<-c("Employment", "Inflation", "RealGDP", "Unemployment")
-value<-round(c(ts1_cast$mean*1000,ts2_cast$mean,ts3_cast$mean,ts4_cast$mean),2)
-#unit<-c("seasonally adjusted monthly change in nonfarm payroll employment to the nearest thousand persons", "seasonally adjusted annualized monthly percentage change in headline CPI rounded to the nearest tenth of a percent", "seasonally adjusted annualized quarter-to-quarter percentage change advance estimate of GDP from the previous final estimate of GDP, rounded to the nearest tenth of a percent", "Unemployment is the seasonally adjusted level of the headline unemployment rate in percentage points rounded to the nearest tenth of a percent")
+value<-round(c(ts1_cast$mean,ts2_cast$mean,ts3_cast$mean,ts4_cast$mean),4)
 forecast<-data.frame(parameter, value)#%, unit)
 colnames(forecast) <- c("Parameter", "Fcast")#, "Desc")
 forecast
